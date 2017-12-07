d65bb59bce2580298c01c7714d0a12dbf3df7875
Martine <marhab21@gmail.com>
2017-12-07 13:49:34 -0800

    Fixing help page coming up from start screen, when the list of sessions is empty.

--- Zendo/AppDelegate.swift
+++ Zendo/AppDelegate.swift
@@ -62,14 +62,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print ("Enter Foreground")
+       // if (!onHelpPage) {
+            switchToHelpPage()
+       // }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        if (!onHelpPage) {
-            switchToHelpPage()
-        }
+       // if (!onHelpPage) {
+       //     switchToHelpPage()
+       // }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
